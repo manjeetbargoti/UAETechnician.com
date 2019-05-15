1e4d90fb9a0edd04e6133b2630107f8c312b91c2
manjeetbargoti <msb.2905@gmail.com>
2019-05-15 12:27:53 +0530

    Deleted All Old Project Files

--- resources/views/404_temp/property_404.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.frontLayout.frontend_design')
-@section('content')
-
-<h1>There is no Property with this name.</h1>
-
-
-@endsection
--- storage/framework/views/4d5b4c48cd0c01d145d574b6732fe3ad10dfcd7a.php
+++
@@ -1,9 +0,0 @@
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="pull-right hidden-xs">
-      Version v0.1.5-beta
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; <script>document.write(new Date().getFullYear());</script> <a href="#">India Property Clinic</a>.</strong> All rights reserved.
-  </footer><?php /**PATH /home/gqufoc2n7p3n/public_html/property.devirvine.com/resources/views/layouts/adminLayout/admin_footer.blade.php ENDPATH**/ ?>
